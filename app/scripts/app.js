41b31c3bae6c9a2ba455fea3b073393628c39a93
fertech <faispuro@rew.ca>
2015-01-16 15:29:26 -0800

    libraries loading twice.:modulerr error in console

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,23 @@
-var taskList = angular.module("TaskList", ['firebase', 'ui.router']);
+var taskListApp = angular.module("TaskListApp", ['firebase', 'ui.router', 'ng-route']);
 
-taskList.controller('Homepage.controller', ['$scope', '$firebase', function($scope,$firebase) {
+taskListApp.controller('Homepage.controller', ['$scope', '$firebase', function($scope,$firebase) {
   var fireData = new Firebase("https://radiant-fire-5902.firebaseio.com/");
   var sync = $firebase(fireData);
   // download the data into a local object
   $scope.data = sync.$asObject();
 }])
+
+taskListApp.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('active', {
+      url: "/",
+      templateUrl: "templates/active.html"
+      // controller: "Homepage.controller"
+    })
+    
+});
