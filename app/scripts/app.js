846628b0f25253c709416454129b0931041f9782
fertech <faispuro@rew.ca>
2015-01-18 21:46:48 -0800

    Basic Styling for list. Adding a static task

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,19 @@
 var taskListApp = angular.module("TaskListApp", ['firebase', 'ui.router', 'ngRoute']);
 
 taskListApp.controller('Homepage.controller', ['$scope', '$firebase', function($scope,$firebase) {
-  var fireData = new Firebase("https://radiant-fire-5902.firebaseio.com/");
-  var sync = $firebase(fireData);
+  var tasksData = new Firebase("https://radiant-fire-5902.firebaseio.com");
+  var sync = $firebase(tasksData);
   // download the data into a local object
-  $scope.data = sync.$asObject();
+  $scope.tasks = sync.$asArray();
+
+  $scope.addTask = function() {
+	$scope.tasks.$add({description:"add task here"})
+   /*	var newTask = $scope.tasks.child("task");
+	newTask.set({
+		description: "My description"
+	});
+	*/
+}
 }])
 
 taskListApp.config(function($stateProvider, $urlRouterProvider) {
